4e33ff1097da841291fc12e1cf2ec83877343159
noah <jsk9106@gmail.com>
2023-08-08 11:31:20 +0900

    jsonModel migration

--- lib/JsonModel/CommunityPost.dart
+++ lib/JsonModel/CommunityPost.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
 
 class CommunityPost with ChangeNotifier {
-  String CommunityPostTopic = null;
-  String CommunityPostTitle = null;
-  String CommunityPostDescription= null;
-  String CommunityPostWriter = null;
-  int CommunityPostLikes = 0;
-  int CommunityPostReply = 0;
-  bool CommunityPostBlack = false;
-
-  //댓글
-  String ReplyImageURL = null;
-  String ReplyName = null;
-  String ReplyDescription = null;
-  String ReplyTime = null;
-  int ReplyLikes = 0;
-  int ReplyOfReply = 0;
-
-  List<String>CommunityPostImageURL = [];
+  // String? CommunityPostTopic;
+  // String? CommunityPostTitle;
+  // String? CommunityPostDescription;
+  // String? CommunityPostWriter;
+  // int CommunityPostLikes = 0;
+  // int CommunityPostReply = 0;
+  // bool CommunityPostBlack = false;
+  //
+  // //댓글
+  // String? ReplyImageURL;
+  // String? ReplyName;
+  // String? ReplyDescription;
+  // String? ReplyTime;
+  // int ReplyLikes = 0;
+  // int ReplyOfReply = 0;
+  //
+  // List<String>CommunityPostImageURL = [];
 }
